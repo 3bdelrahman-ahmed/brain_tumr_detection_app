daaca7b9bd0700b8d6675a2c8e8c7a8febf6ed21
Abdelrahman hagag <abdohagag854@gmail.com>
2025-04-11 02:45:07 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:brain_tumr_detection_app/core/components/cubits/location_cubit/location_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/services/service_locator/service_locator.dart';
