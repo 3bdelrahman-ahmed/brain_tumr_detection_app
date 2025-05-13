5e2767fa8b00dacb1deb82ee5e398ae71c826ccb
Abdelrahman hagag <abdohagag854@gmail.com>
2025-05-13 15:25:59 +0300

    some testing updates

--- lib/core/services/notification_service/notification_service.dart
+++ lib/core/services/notification_service/notification_service.dart
@@ -5,67 +5,69 @@ import 'badge_service.dart';
 
 class PushNotificationService {
   final FirebaseMessaging _fcm = FirebaseMessaging.instance;
-final FlutterLocalNotificationsPlugin _localNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin _localNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
- Future<void> initialize() async {
-  // Init local notifications
-  const AndroidInitializationSettings androidSettings =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
+  Future<void> initialize() async {
+    // Init local notifications
+    const AndroidInitializationSettings androidSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  const InitializationSettings initSettings =
-      InitializationSettings(android: androidSettings);
-  final BadgeService _badgeService = BadgeService.instance;
+    const InitializationSettings initSettings =
+        InitializationSettings(android: androidSettings);
+    final BadgeService _badgeService = BadgeService.instance;
 
-  await _localNotificationsPlugin.initialize(initSettings);
+    await _localNotificationsPlugin.initialize(initSettings);
 
-  // FCM permissions
-  NotificationSettings settings = await _fcm.requestPermission();
+    // FCM permissions
+    NotificationSettings settings = await _fcm.requestPermission();
 
-  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
-    print('User granted permission');
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
 
-    // Get the FCM token
-    String? token = await _fcm.getToken();
-    print("FCM Token: $token");
+      // Get the FCM token
+      String? token = await _fcm.getToken();
+      print("FCM Token: $token");
 
-    // Foreground message
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Foreground message: ${message.data}');
-      if (message.notification != null) {
-        _showLocalNotification(message);
-           _badgeService.updateBadgeVisibility(true);
-      }
-    });
+      // Foreground message
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print('Foreground message: ${message.data}');
+        print('Message title: ${message.notification?.title}');
+        print('Message body: ${message.notification?.body}');
+        if (message.notification != null) {
+          print('Notification title: ${message.notification?.title}');
+          print('Notification body: ${message.notification?.body}'); 
+          _showLocalNotification(message);
+          _badgeService.updateBadgeVisibility(true);
+        }
+      });
 
-    // Background/terminated
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('App opened from notification: ${message.data}');
-    });
+      // Background/terminated
+      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+        print('App opened from notification: ${message.data}');
+      });
+    }
   }
-}
 
-Future<void> _showLocalNotification(RemoteMessage message) async {
- const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
-  'high_importance_channel',
-  'High Importance Notifications',
-  channelDescription: 'This channel is used for important notifications.',
-  importance: Importance.max,
-  priority: Priority.high,
-);
+  Future<void> _showLocalNotification(RemoteMessage message) async {
+    const AndroidNotificationDetails androidDetails =
+        AndroidNotificationDetails(
+      'high_importance_channel',
+      'High Importance Notifications',
+      channelDescription: 'This channel is used for important notifications.',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
 
+    const NotificationDetails platformDetails =
+        NotificationDetails(android: androidDetails);
 
-  const NotificationDetails platformDetails =
-      NotificationDetails(android: androidDetails);
-
-  await _localNotificationsPlugin.show(
-    message.hashCode,
-    message.notification?.title ?? 'Title',
-    message.notification?.body ?? 'Body',
-    platformDetails,
-    payload: 'optional_payload',
-  );
-}
-
-
+    await _localNotificationsPlugin.show(
+      message.hashCode,
+      message.notification?.title ?? 'Title',
+      message.notification?.body ?? 'Body',
+      platformDetails,
+      payload: 'optional_payload',
+    );
+  }
 }
