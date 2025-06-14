e52629c1919f2d064f0d7f8d68c6591f35b4eba3
youssef7221 <2youssef2ibrahim@gmail.com>
2025-06-15 02:08:56 +0300

    edit in comment bottom sheet

--- lib/features/feed/presentation/view/widgets/comments_bottom_sheet_widget.dart
+++ lib/features/feed/presentation/view/widgets/comments_bottom_sheet_widget.dart
@@ -32,90 +32,96 @@ class _CommentsBottomSheetWidgetState extends State<CommentsBottomSheetWidget> {
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
     var cubit = context.read<FeedCubit>();
 
-    return SizedBox(
-      height: 600.h,
-      child: Column(
-        children: [
-          /// COMMENTS LIST
-          Expanded(
-            child: BlocBuilder<FeedCubit, FeedState>(
+    return AnimatedPadding(
+      duration: const Duration(milliseconds: 300),
+      padding: MediaQuery.of(context).viewInsets,
+      curve: Curves.easeOut,
+      child: SizedBox(
+        height: 600.h,
+        child: Column(
+          children: [
+            // COMMENTS LIST
+            Expanded(
+              child: BlocBuilder<FeedCubit, FeedState>(
+                builder: (context, state) {
+                  if (cubit.postComments.isEmpty && state is CommmentsLoading) {
+                    return ListView.separated(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                      itemBuilder: (context, index) => CustomAppShimmer(
+                        height: 80.h,
+                        borderRaduis: 12.r,
+                      ),
+                      separatorBuilder: (context, index) => 12.toHeight,
+                      itemCount: 3,
+                    );
+                  } else if (cubit.postComments.isNotEmpty) {
+                    return ListView.separated(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                      itemBuilder: (context, index) => CommentCardWidget(
+                        comment: cubit.postComments[index],
+                      ).animate().fadeIn(duration: 300.ms),
+                      separatorBuilder: (context, index) => 12.toHeight,
+                      itemCount: cubit.postComments.length,
+                    );
+                  } else {
+                    return Center(
+                      child: CustomEmptyWidget(
+                        title: S.of(context).noCommentsOnThisPostYet,
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+
+            // COMMENT INPUT FIELD
+            BlocBuilder<FeedCubit, FeedState>(
+              buildWhen: (previous, current) => current is CommentTextChanged,
               builder: (context, state) {
-                if (cubit.postComments.isEmpty && state is CommmentsLoading) {
-                  return ListView.separated(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-                    itemBuilder: (context, index) => CustomAppShimmer(
-                      height: 80.h,
-                      borderRaduis: 12.r,
-                    ),
-                    separatorBuilder: (context, index) => 12.toHeight,
-                    itemCount: 3,
-                  );
-                } else if (cubit.postComments.isNotEmpty) {
-                  return ListView.separated(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-                    itemBuilder: (context, index) => CommentCardWidget(
-                      comment: cubit.postComments[index],
-                    ).animate().fadeIn(duration: 300.ms),
-                    separatorBuilder: (context, index) => 12.toHeight,
-                    itemCount: cubit.postComments.length,
-                  );
-                } else {
-                  return Center(
-                    child: CustomEmptyWidget(
-                      title: S.of(context).noCommentsOnThisPostYet,
-                    ),
-                  );
-                }
+                final hasText = state is CommentTextChanged && state.hasText;
+
+                return Container(
+                  color: AppColors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 12.h),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: CustomTextField(
+                          keyboardType: TextInputType.multiline,
+                          controller: cubit.commentController,
+                          onChanged: cubit.validateComment,
+                          validator: (v) => checkFieldValidation(
+                            val: cubit.commentController.text,
+                            fieldName: S.of(context).comment,
+                            fieldType: ValidationType.text,
+                          ),
+                          hintText: S.of(context).writeYourComment,
+                        ),
+                      ),
+                      if (hasText)
+                        Expanded(
+                          child: CustomImageView(
+                            onTap: () => cubit.addComment(widget.postId),
+                            width: 32.w,
+                            height: 32.w,
+                            color: AppColors.typography,
+                            svgPath: AssetsSvg.sendMessage.toSVG(),
+                          ).animate().flipV(duration: 300.ms),
+                        ),
+                    ],
+                  ),
+                );
               },
             ),
-          ),
-
-          /// COMMENT INPUT FIELD
-          BlocBuilder<FeedCubit, FeedState>(
-            buildWhen: (previous, current) => current is CommentTextChanged,
-            builder: (context, state) {
-              final hasText = state is CommentTextChanged && state.hasText;
-
-              return Container(
-                color: AppColors.white,
-                padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 12.h),
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 5,
-                      child: CustomTextField(
-                        keyboardType: TextInputType.multiline,
-                        controller: cubit.commentController,
-                        onChanged: cubit.validateComment,
-                        validator: (v) => checkFieldValidation(
-                          val: cubit.commentController.text,
-                          fieldName: S.of(context).comment,
-                          fieldType: ValidationType.text,
-                        ),
-                        hintText: S.of(context).writeYourComment,
-                      ),
-                    ),
-                    if (hasText)
-                      Expanded(
-                        child: CustomImageView(
-                          onTap: () => cubit.addComment(widget.postId),
-                          width: 32.w,
-                          height: 32.w,
-                          color: AppColors.typography,
-                          svgPath: AssetsSvg.sendMessage.toSVG(),
-                        ).animate().flipV(duration: 300.ms),
-                      ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
